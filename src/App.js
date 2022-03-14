1727a1e9fcdb359468cecb402c91993e42ed76be
Eunice Adriano <abbyeuniceadriano10@gmail.com>
2022-03-14 20:19:44 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
 
+   const [list, setList] = useState('');
+   const [date, setDate] = useState('');
+
+   const handle = () => {
+      localStorage.setItem('list', list);
+      localStorage.setItem('date', date);
+   };
+   const remove = () => {
+      localStorage.removeItem('list');
+      localStorage.removeItem('date');
+   };
+   return (
+      <div className="app">
+      <div className="container">
+      <form onClick={handle} className="form">
+         <input
+          type="text"
+            value={list}
+            onChange={(e) => setList(e.target.value)}
+            className="list"
+         />
+         <input
+          type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className="date"
+         />
+         <div>
+            <button className="submit">Submit</button>
+         </div>
+         <div>
+            <button onClick={remove} className="remove">Remove</button>
+         </div>
+         </form>
+         <section className="result">
+            <div>
+            <p>{localStorage.getItem('date')} {localStorage.getItem('list')} </p>
+            </div>
+         </section>
+         </div>
+      </div>
+   );
+};
 export default App;
